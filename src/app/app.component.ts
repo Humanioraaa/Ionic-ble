5ac2210e6f9991649c5a5e8b1640c11b261c5385
si-deltapyy <sguritno16@gmail.com>
2024-11-28 22:06:36 +0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,5 @@ export class AppComponent {
     { title: 'Graph', url: 'graph', icon: 'paper-plane' },
     { title: 'Info', url: 'info', icon: 'heart' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
